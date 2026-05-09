5370c22979234126e32b58e250cd7c53ab6f4e0a
SpacePanda7077 <spacepanda7077@gmail.com>
2026-05-09 05:45:05 -0400

    removed unused polygon file

--- client/src/game/main.ts
+++ client/src/game/main.ts
@@ -2,14 +2,14 @@ import { init } from "@dimforge/rapier2d-compat";
 import { Game as MainGame } from "./scenes/Game";
 import { Menu } from "./scenes/Menu";
 import { Ui } from "./scenes/Ui";
-import { AUTO, WEBGL, Game, Scale, Types } from "phaser";
+import { AUTO, Game, Scale, Types } from "phaser";
 import PhaserRaycaster from "phaser-raycaster";
 
 // Find out more information about the Game Config at:
 // https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
 await init();
 const config: Types.Core.GameConfig = {
-    type: WEBGL,
+    type: AUTO,
 
     scale: {
         mode: Scale.FIT,
